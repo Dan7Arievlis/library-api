afd78269909a30295f8098e37c35a7e809a0b9dc
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-08-14 19:55:19 -0300

    First commit

---
+++ src/test/java/io/github/dan7arievlis/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dan7arievlis.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
