c2ab29774467a58746e3959272bdff98fed22759
Daniel Neves <73949890+Dan7Arievlis@users.noreply.github.com>
2025-08-19 16:38:07 -0300

    Começo da API de autores

---
+++ src/main/java/io/github/dan7arievlis/libraryapi/controller/AuthorController.java
@@ -0,0 +1,10 @@
+package io.github.dan7arievlis.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("authors")
+public class AuthorController {
+
+}
